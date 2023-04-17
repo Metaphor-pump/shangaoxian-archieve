f0daa7fcdebe8474d77ebf866d8eec3cd9f2e350
metaphor <446376407@qq.com>
2023-04-17 18:47:02 +0800

    feat: 完善标题作者

--- json2md.py
+++ json2md.py
@@ -16,14 +16,12 @@ def parse_comments(markdown, comments, indent=0):
             # 解析根评论
             if 'root_comment' in anchor_comments:
                 root_comment = anchor_comments['root_comment']
-                print('--------root_comment-------')
-                print(root_comment)
                 markdown += f"{indent_str}  - "
-                parse_comments([root_comment], indent + 2)
+                parse_comments(markdown, [root_comment], indent + 2)
             # 解析其他回复
             if 'comments' in anchor_comments:
                 reply_comments = anchor_comments['comments']
-                parse_comments(reply_comments, indent + 2)
+                parse_comments(markdown, reply_comments, indent + 2)
 
 # 加载json文件
 with open('26765290.json', 'r', encoding='utf-8') as f:
@@ -32,10 +30,10 @@ with open('26765290.json', 'r', encoding='utf-8') as f:
 # 解析文章标题和正文
 title = data['question']
 content = data['content']
-
+author = data['author']
 # 构造Markdown格式的文章
-markdown = f"# {title}\n\n{content}\n\n"
-
+markdown = f"- 作者：{author}\n\n{content}\n\n"
+# markdown = f"# {title}\n\n - 作者：{author}\n\n{content}\n\n"
 
 # 解析文章评论并添加到Markdown中
 comments = data['featured_comments']
@@ -43,6 +41,6 @@ parse_comments(markdown, comments)
 markdown += '\n'
 
 # 将Markdown保存到文件中
-with open('document.md', 'w') as f:
+with open('md\\' + title + '.md', 'w', encoding='utf-8') as f:
     f.write(markdown)
 
